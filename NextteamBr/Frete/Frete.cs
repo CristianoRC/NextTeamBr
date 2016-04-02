f64a58ca971fc11168a9672c5d40b2f9ece82242
Cristiano Raffi Cunha <cristianoraffi@bol.com.br>
2016-04-02 14:57:35 -0300

    Banco de dados

--- NextteamBr/Frete/Frete.cs
+++ NextteamBr/Frete/Frete.cs
@@ -4,6 +4,7 @@ namespace NextteamBr
 {
     class Frete
     {
+        public uint ID { get; set; }
         public double KmRodado { get; set; }
         public double DistanciaInicial { get; set; }
         public double DistanciaFinal { get; set; }
