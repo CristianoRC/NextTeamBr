786a60152abe516734a40fa33ca9fc8fdc195182
Cristiano Cunha <cristianoraffi@bol.com.br>
2016-12-06 15:57:22 -0200

    Pacotes

---
+++ NextteamBr/Ferramentas.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace NextteamBr
+{
+	public static class Ferramentas
+	{
+		/// <summary>
+		/// Verificando se é ATS ou ETS2
+		/// </summary>
+		/// <returns></returns>
+		public static string VerificarGame()
+		{
+			string saida = "";
+
+			Process[] processes = Process.GetProcesses();
+			for (int i = 0; i < processes.Length; i++)
+			{
+				Process process = processes[i];
+				if (process.MainWindowTitle.Contains("Euro Truck Simulator 2"))
+				{
+					saida = "Euro Truck Simulator 2";
+				}
+				if (process.MainWindowTitle.Contains("American Truck Simulator"))
+				{
+					saida = "American Truck Simulator";
+				}
+			}
+
+			return saida;
+		}
+
+		public static double CalcularPontuacao(double KmRodado, double Dano, uint Infracoes)
+		{
+			//A cada 20 infrações perde 1 KM.
+			//A cada 30 KM 1 ponto
+			// a cada 0.05 de dano perde 1 KM.
+
+			double saida;
+			double KmPerdido;
+
+			KmPerdido = (Dano / 0.05);
+			KmPerdido += (Infracoes / 20);
+
+			double KmFinal = KmRodado - KmPerdido;
+			saida = (KmFinal / 30);
+
+			return saida;
+		}
+	}
+}
---
+++ NextteamBr/Frete/ControllerFrete.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace NextteamBr
+{
+	class ControllerFrete
+	{
+		public static string SalvarFrete(Frete InformacoesFrete)
+		{
+			string Saida;
+
+			Saida = JsonConvert.SerializeObject(InformacoesFrete);
+
+			System.IO.StreamWriter sw;
+			sw = new System.IO.StreamWriter("Informacoes.JSON");
+
+			sw.WriteLine(Saida);
+
+			return Saida;
+		}
+	}
+}
