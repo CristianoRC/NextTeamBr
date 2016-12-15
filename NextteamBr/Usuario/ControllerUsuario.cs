68a507d28c703ba8a7b4b1ec0237bb87b4b1e4e1
Cristiano Cunha <cristianoraffi@bol.com.br>
2016-12-15 10:00:18 -0200

    Refatoracao

--- NextteamBr/Usuario/ControllerUsuario.cs
+++ NextteamBr/Usuario/ControllerUsuario.cs
@@ -7,50 +7,37 @@ using System.Text;
 
 namespace NextteamBr
 {
-    public static class ControllerUsuario
-    {
-        public static string Logar(Usuario uso)
-        {
-            bool saida;
-            string StrJSON;
-            try
-            {
-                StrJSON = JsonConvert.SerializeObject(uso);
+	public static class ControllerUsuario
+	{
+		public static string Logar(Usuario uso)
+		{
+			string StrJSON;
+			try
+			{
+				StrJSON = JsonConvert.SerializeObject(uso);
 
-                string url = "http://painel.nextteambr.com.br/loginapp.php";
-                HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(url);
-                ASCIIEncoding encoding = new ASCIIEncoding();
-                string postData = StrJSON;
-                byte[] data = encoding.GetBytes(postData);
-                httpWReq.Method = "POST";
-                httpWReq.ContentType = "application / json; charset = utf-8";
-                httpWReq.ContentLength = data.Length;
-                httpWReq.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
-                using (Stream stream = httpWReq.GetRequestStream())
-                {
-                    stream.Write(data, 0, data.Length);
-                }
-                HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
-                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+				string url = "http://painel.nextteambr.com.br/loginapp.php";
+				HttpWebRequest httpWReq = (HttpWebRequest)WebRequest.Create(url);
+				ASCIIEncoding encoding = new ASCIIEncoding();
+				string postData = StrJSON;
+				byte[] data = encoding.GetBytes(postData);
+				httpWReq.Method = "POST";
+				httpWReq.ContentType = "application / json; charset = utf-8";
+				httpWReq.ContentLength = data.Length;
+				httpWReq.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+				using (Stream stream = httpWReq.GetRequestStream())
+				{
+					stream.Write(data, 0, data.Length);
+				}
+				HttpWebResponse response = (HttpWebResponse)httpWReq.GetResponse();
+				string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
 
-                return responseString;
-
-                /*
-                if ()
-                {
-                    saida = true;
-                }
-                else
-                {
-                    saida = false;
-                }
-
-                */
-            }
-            catch (System.Exception e)
-            {
-                return "Erro:" + e.Message;
-            }
-        }
-    }
+				return responseString;
+			}
+			catch (System.Exception e)
+			{
+				return "Erro:" + e.Message;
+			}
+		}
+	}
 }
