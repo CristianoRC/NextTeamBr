3e869f5c1d853f57e5f50d8bf901c169c262d12a
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-12-30 10:57:33 -0200

    Correções na tela de cadastro

--- Site/Motorista/js/index.js
+++ Site/Motorista/js/index.js
@@ -4,15 +4,17 @@ var button_login = $('#toggle-login');
 var button_register = $('#toggle-register');
 var box = $('.box');
 
-$(function() {
+$(function () {
 
-  register.hide();
+  login.hide();
+  button_login.hide();
+  button_register.hide();
 
-  button_register.click(function() {
+  button_register.click(function () {
     login.slideUp(1000);
     register.slideDown(1000);
   });
-  button_login.click(function() {
+  button_login.click(function () {
     register.slideUp(1000);
     login.slideDown(1000);
   });
